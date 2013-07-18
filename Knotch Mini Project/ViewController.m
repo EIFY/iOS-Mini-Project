f93db920adc3ee340ab8d8a2b9640be95438f0ea
Jason Chou <chuanchih@gmail.com>
2013-07-17 23:16:18 -0400

    Font color adjustment, profilePictureView, nameLabel roughly placed

--- Knotch Mini Project/ViewController.m
+++ Knotch Mini Project/ViewController.m
@@ -26,6 +26,9 @@
     
     CGFloat navbarHeight = 42.5;
     CGFloat colorStripHeight = 232/2;//There is a 1-px strip of lighter shade below in the sample rendering. Not sure if it's intentional...
+    CGFloat nameLabelHeight = 144/2;
+    UIColor* nameFontColor = [UIColor colorWithRed:39.0/255 green:49.0/255 blue:55.0/255 alpha:1.0];
+    //NSMutableDictionary AllerTextAttributes
     
     UINavigationBar* usernameNavbar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, 320, navbarHeight)];
     [self.view addSubview:usernameNavbar];
@@ -36,7 +39,8 @@
     
     [usernameNavbar setBackgroundColor:[UIColor whiteColor]];
     usernameNavbar.tintColor = [UIColor whiteColor];
-    usernameNavbar.titleTextAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIColor blackColor], UITextAttributeTextColor, [UIFont fontWithName:@"Aller" size:15], UITextAttributeFont, [UIColor clearColor], UITextAttributeTextShadowColor, nil];
+    usernameNavbar.titleTextAttributes = [NSDictionary dictionaryWithObjectsAndKeys:nameFontColor, UITextAttributeTextColor, [UIFont fontWithName:@"Aller" size:15], UITextAttributeFont, [UIColor clearColor], UITextAttributeTextShadowColor, nil];
+    //RGB = (39, 49, 55)
     [usernameNavbar setTitleVerticalPositionAdjustment:3 forBarMetrics:UIBarMetricsDefault];
     
     usernameNavbar.layer.masksToBounds = YES;//Eliminates shadow
@@ -51,10 +55,33 @@
     
     [self.view insertSubview:containerScrollView belowSubview:usernameNavbar];
     
+    UIImageView* profilePictureView = [[UIImageView alloc] initWithFrame:CGRectMake(31.0/2, navbarHeight+192/2, 167.0/2, 153.0/2)];//Bizarre layout, but it's what it is...
+    [containerScrollView insertSubview:profilePictureView aboveSubview:colorStripView];
     
+    profilePictureView.backgroundColor = [UIColor blackColor];
+    
+    UILabel* nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, navbarHeight + colorStripHeight, 320, nameLabelHeight)];
+    
+    nameLabel.text = @"Anda Ganscaç";
+    nameLabel.textAlignment = NSTextAlignmentCenter;
+    nameLabel.font = [UIFont fontWithName:@"Aller" size:17.5];
+    nameLabel.textColor = nameFontColor;
+    
+    //NSMutableAttributedString *attributedString;
+    //attributedString = [[NSMutableAttributedString alloc] initWithString:@"Anda Gansca"];
+    //[attributedString addAttribute:NSKernAttributeName value:@0.5 range:NSMakeRange(0, 10)];
+    //[attributedString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"Aller" size:17.5] range:NSMakeRange(0, 10)];
+    //[attributedString addAttribute:<#(NSString *)#> value:<#(id)#> range:<#(NSRange)#>]
+    
+    //nameLabel.attributedText = attributedString;
+    
+    //nameLabel.shadowColor = [UIColor blackColor];
+    //nameLabel.shadowOffset = CGSizeMake(0, 1);
+    
+    [containerScrollView insertSubview:nameLabel belowSubview:colorStripView];
     
     //UIOffsetMake(0, 0), UITextAttributeTextShadowOffset
-    /*
+    
     for (NSString* family in [UIFont familyNames])
     {
         NSLog(@"%@", family);
@@ -64,7 +91,7 @@
             NSLog(@"  %@", name);
         }
     }
-    */
+    
 }
 
 - (void)didReceiveMemoryWarning
